e1ebc5dc594347e284e8d4b127b41a7f8f81f440
Brad Trammell <bradjtrammell@gmail.com>
2022-02-06 17:14:09 -0500

    Update TripleDES.php

--- whmcs/modules/servers/w3esxi/libs/Crypt/TripleDES.php
+++ whmcs/modules/servers/w3esxi/libs/Crypt/TripleDES.php
@@ -244,7 +244,7 @@ class Crypt_TripleDES {
      * @return Crypt_TripleDES
      * @access public
      */
-    function Crypt_TripleDES($mode = CRYPT_DES_MODE_CBC)
+    function __construct($mode = CRYPT_DES_MODE_CBC)
     {
         if ( !defined('CRYPT_DES_MODE') ) {
             switch (true) {
